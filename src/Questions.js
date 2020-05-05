31414b51fbf0a211be589b891a4bb490006f4693
kthaovd13 <58517232+kthaovd13@users.noreply.github.com>
2020-05-05 18:48:46 -0500

    added sprint questions

---
+++ src/Questions.js
@@ -0,0 +1,8 @@
+// 1. In 1-2 sentences, explain what React's useRouteMatch hook is used for
+//Reacts useRouteMatch allows you to change a single parameter in the URL without having to change the whole thing via nested routes.
+
+// 2. How would you explain form validation to someone who has never programmed before?
+//If you are typing your email without the "@", the form informs you that that is not a valid email.
+
+// 3. In 1-2 sentences, define end to end testing.
+//It focuses on UI and mimics user interaction with an app, making sure everyting works as they should like scrolling and button clicks.
